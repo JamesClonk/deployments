5d051a8c6897b1c8b43f1abb87ab78782e4fffbc
JamesClonk <jamesclonk@jamesclonk.ch>
2020-06-20 12:57:42 +0200

    add kapp gc

--- cf-env/diff.sh
+++ cf-env/diff.sh
@@ -3,4 +3,5 @@ set -e
 source ../setup.sh
 
 # diff
+kapp -y app-change list --app ${KUBE_APP_NAME}
 kubectl kustomize ${KUBE_ENVIRONMENT} | envsubst -no-unset -no-empty | kapp deploy --app ${KUBE_APP_NAME} --diff-changes --diff-run -f -
--- kuard/deploy.sh
+++ kuard/deploy.sh
@@ -5,4 +5,5 @@ source ../setup.sh
 # deploy
 echo "deploying [${KUBE_APP_NAME}] to [${KUBE_ENVIRONMENT}] ..."
 kubectl kustomize ${KUBE_ENVIRONMENT} | envsubst -no-unset -no-empty | kapp -y deploy --app ${KUBE_APP_NAME} --diff-changes -f -
+kapp -y app-change garbage-collect --app ${KUBE_APP_NAME} --max 5
 echo "done!"
