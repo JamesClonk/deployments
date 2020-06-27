edcbcc3fa48751ef83088f0e651d9da3884e217d
JamesClonk <jamesclonk@jamesclonk.ch>
2020-06-27 15:28:05 +0200

    make dashboard available behind TLS+basic-auth

---
+++ dashboard/diff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+source ../setup.sh
+
+# diff
+kapp app-change list --app ${KUBE_APP_NAME}
+kubectl kustomize ${KUBE_ENVIRONMENT} | envsubst -no-unset -no-empty | kapp deploy --app ${KUBE_APP_NAME} --diff-changes --diff-run -f -
