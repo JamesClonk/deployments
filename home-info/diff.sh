9bc1474239980f0ca22d06d8a8f800f8646ede23
JamesClonk <jamesclonk@jamesclonk.ch>
2020-07-15 22:10:28 +0200

    migrate home-info-dashboard and kuard to ytt templates

--- home-info/diff.sh
+++ home-info/diff.sh
@@ -4,4 +4,4 @@ source ../setup.sh
 
 # diff
 kapp app-change list --app ${KUBE_APP_NAME}
-kubectl kustomize ${KUBE_ENVIRONMENT} | envsubst -no-unset -no-empty | kapp deploy --app ${KUBE_APP_NAME} ${KAPP_DIFF} --diff-run -f -
+cat values.yml | envsubst -no-unset -no-empty | ytt --strict --ignore-unknown-comments -f templates -f - | kapp deploy --app ${KUBE_APP_NAME} ${KAPP_DIFF} --diff-run -f -
--- kuard/deploy.sh
+++ kuard/deploy.sh
@@ -3,7 +3,7 @@ set -e
 source ../setup.sh
 
 # deploy
-echo "deploying [${KUBE_APP_NAME}] to [${KUBE_ENVIRONMENT}] ..."
-kubectl kustomize ${KUBE_ENVIRONMENT} | envsubst -no-unset -no-empty | kapp -y deploy --app ${KUBE_APP_NAME} ${KAPP_DIFF} -f -
+echo "deploying [${KUBE_APP_NAME}] ..."
+cat values.yml | envsubst -no-unset -no-empty | ytt --strict --ignore-unknown-comments -f templates -f - | kapp -y deploy --app ${KUBE_APP_NAME} ${KAPP_DIFF} -f -
 kapp -y app-change garbage-collect --app ${KUBE_APP_NAME} --max 3
 echo "done!"
