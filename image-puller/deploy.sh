42403cca4dc0d562df7d57b716c22b258bb52eb5
JamesClonk <jamesclonk@jamesclonk.ch>
2020-08-21 23:09:51 +0200

    migrate to k8s secret

--- image-puller/deploy.sh
+++ image-puller/deploy.sh
@@ -3,6 +3,10 @@ set -e
 source ../setup.sh
 
 # deploy
+echo "deleting [${KUBE_APP_NAME}] ..."
+kapp -y delete --app ${KUBE_APP_NAME}
+
+# deploy
 echo "deploying [${KUBE_APP_NAME}] ..."
 cat values.yml | envsubst -no-unset -no-empty | ytt --strict --ignore-unknown-comments -f templates -f - | kapp -y deploy --app ${KUBE_APP_NAME} ${KAPP_DIFF} -f -
 kapp -y app-change garbage-collect --app ${KUBE_APP_NAME} --max 3
