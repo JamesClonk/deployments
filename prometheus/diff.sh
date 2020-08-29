a3405a41d081eb460c0856942661df621569d006
JamesClonk <jamesclonk@jamesclonk.ch>
2020-08-29 21:56:12 +0200

    add backman scrape_config

--- prometheus/diff.sh
+++ prometheus/diff.sh
@@ -4,4 +4,5 @@ source ../setup.sh
 
 # diff
 kapp app-change list --app ${KUBE_APP_NAME}
-cat values.yml | envsubst -no-unset -no-empty | ytt --ignore-unknown-comments -f templates -f - | kbld -f - | kapp deploy --app ${KUBE_APP_NAME} ${KAPP_DIFF} --diff-run -f -
+cat values.yml | envsubst -no-unset -no-empty | ytt --ignore-unknown-comments -f templates -f - |
+  sed "s/__BACKMAN_USERNAME__/${BACKMAN_USERNAME}/" | kbld -f - | kapp deploy --app ${KUBE_APP_NAME} ${KAPP_DIFF} --diff-run -f -
