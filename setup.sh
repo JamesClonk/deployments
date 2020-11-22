277e96bf6b346fe7ceb05bdd2b4a5f09b092ff2b
JamesClonk <jamesclonk@jamesclonk.ch>
2020-11-22 10:44:07 +0100

    bump irvisualizer

--- setup.sh
+++ setup.sh
@@ -31,10 +31,10 @@ export PATH="$HOME/bin:$PATH"
 # install tools
 install_tool "kubectl" "https://storage.googleapis.com/kubernetes-release/release/v1.18.8/bin/linux/amd64/kubectl"
 install_tool "envsubst" "https://github.com/JamesClonk/envsubst/releases/download/v1.1.1/envsubst_1.1.1_Linux-64bit"
-install_tool "kapp" "https://github.com/k14s/kapp/releases/download/v0.33.0/kapp-linux-amd64"
+install_tool "kapp" "https://github.com/k14s/kapp/releases/download/v0.34.0/kapp-linux-amd64"
 install_tool "ytt" "https://github.com/k14s/ytt/releases/download/v0.30.0/ytt-linux-amd64"
-install_tool "vendir" "https://github.com/k14s/vendir/releases/download/v0.10.0/vendir-linux-amd64"
-install_tool "kbld" "https://github.com/k14s/kbld/releases/download/v0.24.0/kbld-linux-amd64"
+install_tool "vendir" "https://github.com/k14s/vendir/releases/download/v0.12.0/vendir-linux-amd64"
+install_tool "kbld" "https://github.com/k14s/kbld/releases/download/v0.27.0/kbld-linux-amd64"
 install_tool "kwt" "https://github.com/k14s/kwt/releases/download/v0.0.6/kwt-linux-amd64"
 install_tool_from_tarball "k9s" "https://github.com/derailed/k9s/releases/download/v0.21.7/k9s_Linux_x86_64.tar.gz"
 
